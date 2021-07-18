91d821013cb50741ba68f1b7861c90c04b09ebd2
Benjamin Hipple <bhipple@protonmail.com>
2021-07-18 19:06:08 -0400

    Bind Fewer Keys on the Thumbs

--- flash.sh
+++ flash.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env sh
- ./keyboards/handwired/dactyl_manuform/5x6/keymaps/bhipple/regen.py && nix-shell -I nixpkgs=$HOME/src/nixpkgs --run 'qmk flash'
-
+./keyboards/handwired/dactyl_manuform/5x6/keymaps/bhipple/regen.py && nix-shell --run 'qmk flash'
