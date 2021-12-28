066ff0cd26f7cda25f9ab88ff5b22985b582c263
Benjamin Hipple <bhipple@protonmail.com>
2021-12-27 22:27:15 -0500

    flash script

--- flash.sh
+++ flash.sh
@@ -2,4 +2,6 @@
 set -euxo pipefail
 ./keyboards/handwired/dactyl_manuform/5x6/keymaps/bhipple/regen.py
 ./keyboards/bastardkb/scylla/keymaps/bhipple/regen.py
-nix-shell --run 'qmk flash'
+
+#nix-shell --run 'qmk flash -km bhipple -kb bastardkb/scylla'
+nix-shell --run 'qmk flash -km bhipple -kb handwired/dactyl_manuform/5x6'
--- keyboards/handwired/dactyl_manuform/5x6/keymaps/bhipple/rules.mk
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/bhipple/rules.mk
@@ -2,5 +2,10 @@ BOOTLOADER = atmel-dfu
 
 AUTO_SHIFT_ENABLE = yes
 
+# Slimming down firmware size
+MOUSEKEY_ENABLE = no
+EXTRAKEY_ENABLE = yes
+RGB_MATRIX_ENABLE = no
+
 generated.h: regen.py keymap.org
 	./regen.py
