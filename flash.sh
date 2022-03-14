c18f86bec872856b4b507bd86d1202b0521fa81d
Benjamin Hipple <bhipple@protonmail.com>
2022-03-14 11:24:46 -0400

    Emacs keybinding for next/prev tab

--- flash.sh
+++ flash.sh
@@ -3,5 +3,5 @@ set -euxo pipefail
 ./keyboards/handwired/dactyl_manuform/5x6/keymaps/bhipple/regen.py
 ./keyboards/bastardkb/scylla/keymaps/bhipple/regen.py
 
-#nix-shell --run 'qmk flash -km bhipple -kb bastardkb/scylla'
-nix-shell --run 'qmk flash -km bhipple -kb handwired/dactyl_manuform/5x6'
+nix-shell --run 'qmk flash -km bhipple -kb bastardkb/scylla'
+#nix-shell --run 'qmk flash -km bhipple -kb handwired/dactyl_manuform/5x6'
