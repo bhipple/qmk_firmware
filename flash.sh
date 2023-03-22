1c73800c608b013f639c39a26db824a630773e73
Benjamin Hipple <bhipple@protonmail.com>
2023-03-21 21:43:05 -0400

    Cleanup flash

--- flash.sh
+++ flash.sh
@@ -7,7 +7,7 @@ make git-submodule
 ./keyboards/bastardkb/charybdis/4x6/keymaps/bhipple/regen.py charybdis
 ./keyboards/bastardkb/scylla/keymaps/bhipple/regen.py scylla
 
-if echo "$1 $(hwinfo)" | grep -i Scylla; then nix-shell --run 'qmk flash -km bhipple -kb bastardkb/scylla/v1/elitec'; fi
-if echo "$1 $(hwinfo)" | grep -i Charybdis; then nix-shell --run 'qmk flash -km bhipple -kb bastardkb/charybdis/4x6/v1/elitec'; fi
+if hwinfo | grep -i Scylla; then nix-shell --run 'qmk flash -km bhipple -kb bastardkb/scylla/v1/elitec'; fi
+if hwinfo | grep -i Charybdis; then nix-shell --run 'qmk flash -km bhipple -kb bastardkb/charybdis/4x6/v1/elitec'; fi
 
 #nix-shell --run 'qmk flash -km bhipple -kb handwired/dactyl_manuform/5x6'
