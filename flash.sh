fab30acf3f090c6757a338a509bf0674397a045b
Benjamin Hipple <bhipple@protonmail.com>
2023-06-03 14:21:12 -0400

    Fix flash script

--- flash.sh
+++ flash.sh
@@ -2,7 +2,7 @@
 #!nix-shell -i zsh
 set -euxo pipefail
 
-make git-submodule
+nix-shell -p qmk --run 'make git-submodules'
 
 ./keyboards/bastardkb/charybdis/4x6/keymaps/bhipple/regen.py charybdis
 ./keyboards/bastardkb/scylla/keymaps/bhipple/regen.py scylla
@@ -10,5 +10,3 @@ make git-submodule
 # TODO: fix the nixos udev rules and drop sudo
 if hwinfo | grep -i Scylla; then nix-shell --run 'sudo qmk flash -km bhipple -kb bastardkb/scylla/v1/elitec'; fi
 if hwinfo | grep -i Charybdis; then nix-shell --run 'sudo qmk flash -km bhipple -kb bastardkb/charybdis/4x6/v1/elitec'; fi
-
-#nix-shell --run 'qmk flash -km bhipple -kb handwired/dactyl_manuform/5x6'
