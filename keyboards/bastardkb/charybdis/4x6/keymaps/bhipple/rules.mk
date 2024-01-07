f9a14d5cc6eea98c8e6f2ff5a04f5a1a74a45b2f
Benjamin Hipple <bhipple@protonmail.com>
2024-01-07 10:21:26 -0500

    Cleanup setup

--- keyboards/bastardkb/charybdis/4x6/keymaps/bhipple/rules.mk
+++ keyboards/bastardkb/charybdis/4x6/keymaps/bhipple/rules.mk
@@ -4,6 +4,3 @@ EXTRAKEY_ENABLE = yes
 
 # Slimming down firmware size
 RGB_MATRIX_ENABLE = no
-
-generated.h: regen.py keymap.org
-	./regen.py charybdis
--- keyboards/bastardkb/scylla/keymaps/bhipple/rules.mk
+++ keyboards/bastardkb/scylla/keymaps/bhipple/rules.mk
@@ -4,6 +4,3 @@ AUTO_SHIFT_ENABLE = no
 MOUSEKEY_ENABLE = no
 EXTRAKEY_ENABLE = yes
 RGB_MATRIX_ENABLE = no
-
-generated.h: regen.py keymap.org
-	./regen.py
