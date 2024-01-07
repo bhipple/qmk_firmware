0f15ae4802c83abaef00bff254be34c94202d908
Benjamin Hipple <bhipple@protonmail.com>
2024-01-06 23:12:58 -0500

    descope sudo

--- flash.sh
+++ flash.sh
@@ -7,6 +7,8 @@ nix-shell -p qmk --run 'make git-submodules'
 ./keyboards/bastardkb/charybdis/4x6/keymaps/bhipple/regen.py charybdis
 ./keyboards/bastardkb/scylla/keymaps/bhipple/regen.py scylla
 
-# TODO: fix the nixos udev rules and drop sudo
-if hwinfo | grep -i Scylla; then nix-shell --run 'sudo qmk flash -km bhipple -kb bastardkb/scylla/v1/elitec'; fi
-if hwinfo | grep -i Charybdis; then nix-shell --run 'sudo qmk flash -km bhipple -kb bastardkb/charybdis/4x6/v1/elitec'; fi
+if hwinfo | grep -i Scylla; then kb=bastardkb/scylla/v1/elitec; fi
+if hwinfo | grep -i Charybdis; then kb=bastardkb/charybdis/4x6/v1/elitec; fi
+
+nix-shell --run "qmk compile -km bhipple -kb $kb -t out.hex"
+nix-shell --run "sudo qmk flash out.hex"  # TODO: fix the nixos udev rules and drop sudo
