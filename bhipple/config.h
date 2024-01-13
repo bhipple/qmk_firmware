7c767e8af4d09286ba1b534ab4223c217aef4c37
Benjamin Hipple <bhipple@protonmail.com>
2024-01-13 11:37:16 -0500

    Qwerty keys

--- bhipple/config.h
+++ bhipple/config.h
@@ -1,5 +1,9 @@
 #define TAPPING_TERM 150
+
+// If I press and hold a key for longer than TAPPING_TERM but do not press another key, send the original key
+// https://docs.qmk.fm/#/tap_hold?id=retro-tapping
 #define RETRO_TAPPING
 
+// https://docs.qmk.fm/#/tap_hold?id=permissive-hold
 #undef PERMISSIVE_HOLD
 #undef PERMISSIVE_HOLD_PER_KEY
