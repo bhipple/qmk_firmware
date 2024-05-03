04e363759ec5c1e3b8a1f92e2b17254411ab89ad
Benjamin Hipple <bhipple@protonmail.com>
2024-05-03 09:27:13 -0400

    Change mac layout to colemak

--- keyboards/keychron/v8/ansi_encoder/keymaps/default/keymap.c
+++ keyboards/keychron/v8/ansi_encoder/keymaps/default/keymap.c
@@ -19,7 +19,7 @@
 // clang-format off
 
 enum layers{
-    MAC_BASE,
+    Colemak,
     WIN_BASE,
     _FN1,
     _FN2,
@@ -30,12 +30,12 @@ enum layers{
 #define KC_FLXP LGUI(KC_E)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [MAC_BASE] = LAYOUT_ansi_69(
+    [Colemak] = LAYOUT_ansi_69(
         KC_ESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
-        KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
-        KC_CAPS, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,           KC_HOME,
-        KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
-        KC_LCTL, KC_LOPT,  KC_LCMD,           KC_SPC,           MO(_FN1), MO(_FN3),         KC_SPC,            KC_RCMD,            KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_TAB,  KC_Q,     KC_W,     KC_F,    KC_P,    KC_B,    KC_J,     KC_L,    KC_U,    KC_Y,    KC_QUOT,  KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
+        KC_CAPS, KC_A,     KC_R,     KC_S,    KC_T,    KC_G,              KC_K,    KC_N,    KC_E,    KC_I,     KC_O,     KC_SCLN,  KC_ENT,           KC_HOME,
+        KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_D,    KC_V,     KC_V,    KC_M,    KC_H,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
+        KC_LCTL, KC_LWIN,  KC_LALT,           KC_SPC,           MO(_FN1), MO(_FN3),         KC_SPC,            KC_RALT,            KC_LEFT, KC_DOWN, KC_RGHT),
 
     [WIN_BASE] = LAYOUT_ansi_69(
         KC_ESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [MAC_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [Colemak] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [WIN_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [_FN1]   = {ENCODER_CCW_CW(RM_VALD, RM_VALU)},
     [_FN2]   = {ENCODER_CCW_CW(RM_VALD, RM_VALU)},
