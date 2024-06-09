de024c203730190909ba99fcc9d28d8bda238e45
Benjamin Hipple <bhipple@protonmail.com>
2024-06-08 20:15:10 -0400

    Import voyager as-is

--- keyboards/zsa/voyager/keymaps/bhipple/config.h
+++ keyboards/zsa/voyager/keymaps/bhipple/config.h
@@ -5,3 +5,9 @@
 #define RAW_USAGE_PAGE 0xFF60
 #define RAW_USAGE_ID 0x61
 #define RGB_MATRIX_STARTUP_SPD 60
+
+// Keep all LEDs dark by default; layer indicators in keymap.c light specific keys
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
+#define RGB_MATRIX_DEFAULT_HUE 0
+#define RGB_MATRIX_DEFAULT_SAT 0
+#define RGB_MATRIX_DEFAULT_VAL 0
